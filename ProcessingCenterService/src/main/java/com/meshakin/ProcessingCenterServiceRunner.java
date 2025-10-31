1dda100bd8416e9085b9592ad0d756d5f656289a
Мешакин Максим Александрович <mesakinmaksim@gmail.com>
2025-10-31 05:20:05 +0300

    changed log directory, configured keycloak

--- ProcessingCenterService/src/main/java/com/meshakin/ProcessingCenterServiceRunner.java
+++ ProcessingCenterService/src/main/java/com/meshakin/ProcessingCenterServiceRunner.java
@@ -1,5 +1,7 @@
 package com.meshakin;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -13,6 +15,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableRetry
 public class ProcessingCenterServiceRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(ProcessingCenterServiceRunner.class);
+
 
     public static void main(String[] args) throws Exception {
         var context = SpringApplication.run(ProcessingCenterServiceRunner.class, args);
